295d9d461fa1c81c12e8d128e6113f1fc2c72125
kattypatty <kat.patigorez@gmail.com>
2024-11-08 16:03:44 -0800

    add changes to base_page.py, requirements.txt

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,3 +1,6 @@
+from selenium.webdriver.support.ui import WebDriverWait as wait
+from selenium.webdriver.support import expected_conditions as EC
+
 class BasePage:
     def __init__(self, driver, url):
         self.driver = driver
@@ -5,4 +8,27 @@ class BasePage:
 
     def open_url(self):
         self.driver.get(self.url)
+        return self
         
+    # Methods that will be helpful to find element on the webpage and work with them
+    def element_is_visible(self, locator, timeout=5):
+        return wait(self.driver, timeout).until(EC.visibility_of_element_located(locator))
+
+    def element_are_visible(self, locator, timeout=5):
+        return wait(self.driver, timeout).until(EC.visibility_of_all_elements_located(locator))
+
+    def element_is_present(self, locator, timeout=5):
+        return wait(self.driver, timeout).until(EC.presence_of_element_located(locator))
+
+    def element_are_present(self, locator, timeout=5):
+        return wait(self.driver, timeout).until(EC.presence_of_all_elements_located(locator))
+
+    def element_is_not_visible(self, locator, timeout=5):
+        return wait(self.driver, timeout).until(EC.invisibility_of_element_located(locator))
+
+    def element_is_clickable(self, locator, timeout=5):
+        return wait(self.driver, timeout).until(EC.element_to_be_clickable(locator))
+
+    def find_the_element(self, element):
+        self.driver.execute_script("argument[0].scrollIntoView();".element)
+
